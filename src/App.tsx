3b616b8d94fed8951ed4deb35e667408ae91056d
Antonio Victor <vgoncalvescarvalho@outlook.com>
2025-11-13 01:55:24 -0300

    feat:sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { columns } from "./components/ProductTable/columns"
-import { DataTable } from "./components/ProductTable/DataTable"
-import { products } from "./types/Product"
-
+import {
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom"
+import Sidebar from "./components/Sidebar/Sidebar"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+import Dashboard from "./pages/Dashboard"
 function App() {
   return (
-    <div className="w-full flex min-h-svh flex-col items-center justify-center">
-      <DataTable columns={columns} data={products}/>
+    <div className='w-full flex min-h-svh justify-between bg-gray-200'>
+      <Sidebar />
+      <div className='w-full'>
+        <Outlet />
+      </div>
+      {/* <DataTable columns={columns} data={products} /> */}
     </div>
   )
 }
 
-export default App
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='/dashboard' index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+export default AppRoutes
+
+library.add(fab, fas, far)
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className='w-full'>
+      <h1 className='text-3xl mt-10 ml-15 font-medium'>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
